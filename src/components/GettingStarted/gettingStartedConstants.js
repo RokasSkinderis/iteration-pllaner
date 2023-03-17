cd053d7ed74843692fa64ea871a0cf7ffc12c1f1
DestytojasRokas <rokas.skinderis@bit.lt>
2023-03-17 14:26:08 +0200

    added constants file

---
+++ src/components/GettingStarted/gettingStartedConstants.js
@@ -0,0 +1,21 @@
+export const GettingStartedListItems = {
+    TEAMS: 'Teams',
+    DEVELOPERS: 'Developers',
+    ITERATIONS: 'Iterations',
+}
+
+export const TeamsTableOrder = [
+    {title: '', key: 'chevron'},
+    {title: 'Name', key: 'name'},
+    {title: 'Scrum Master', key: 'scrumMaster'},
+    {title: 'Date Created', key: 'dateCreated'},
+    {title: 'Actions', key: 'actions'},
+]
+
+export const DevelopersTableOrder = [
+    {title: 'Name', key: 'name'},
+    {title: 'Domain', key: 'domain'},
+    {title: 'Based In', key: 'basedIn'},
+    {title: 'Date Created', key: 'dateCreated'},
+    {title: 'Actions', key: 'actions'},
+]
